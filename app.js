a306d72da722ecd949584d1939c9a2b2cc193b5e
Swapnesh Sinha <ssi268@sapient.com>
2016-10-07 13:35:22 +0530

    POC for momentjs locale

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+	app = express(),
+	opn = require('opn'),
+	path = require('path'),
+	PORT = 8000;
+
+app.use('/public', express.static('public'));
+
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+app.listen(PORT,function() {
+	console.log('Server started at port ', PORT);
+	opn('http://localhost:' +PORT);
+})
