ac56d33c6bdbdcdf94fe76515ba112320c944f19
Swapnesh Sinha <swapnesh20032003@gmail.com>
2016-10-07 15:02:18 +0530

    3 - Added route for CDN and cdn page

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname + '/index.html'));
 })
 
+app.get('/cdn', function(req, res){
+	res.sendFile(path.join(__dirname + '/index-cdn.html'));
+})
+
 app.listen(PORT,function() {
 	console.log('Server started at port ', PORT);
 	opn('http://localhost:' +PORT);
